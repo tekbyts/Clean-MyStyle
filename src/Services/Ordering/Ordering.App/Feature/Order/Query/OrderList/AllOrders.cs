94b2d14b33ebf5a40334bfc2104fb485ce843450
Thiru Sowrirajan <tekbyts@gmail.com>
2022-08-28 09:39:43 -0400

    add ignored files to source control

---
+++ src/Services/Ordering/Ordering.App/Feature/Order/Query/OrderList/AllOrders.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using MediatR;
+using Ordering.Contracts.Persist;
+using Ordering.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Feature.Order.Query.OrderList
+{
+    public class AllOrdersQuery : IRequest<List<OrderVm>>
+    {
+        public AllOrdersQuery()
+        {
+        }
+    }
+
+    public class AllOrdersQueryHandler : IRequestHandler<AllOrdersQuery, List<OrderVm>>
+    {
+        private readonly IOrderUow _orderUow;
+        private readonly IMapper _mapper;
+        public AllOrdersQueryHandler(IOrderUow orderUow,
+            IMapper mapper)
+        {
+            _orderUow = orderUow;
+            _mapper = mapper;
+        }
+        public async Task<List<OrderVm>> Handle(AllOrdersQuery request, CancellationToken cancellationToken)
+        {
+            // fetch orders
+            var orders = await _orderUow.GetAllAsync();
+            return _mapper.Map<List<OrderVm>>(orders);
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.App/Feature/Order/Query/OrderList/OrdersByUsername.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using MediatR;
+using Ordering.Contracts.Persist;
+using Ordering.Domain.Model;
+
+namespace Ordering.Application.Feature.Order.Query.OrderList
+{
+    public class OrdersByUsernameQuery : IRequest<List<OrderVm>>
+    {
+        public string Username { get; set; }
+
+        public OrdersByUsernameQuery(string username)
+        {
+            Username = username;
+        }
+    }
+
+    public class OrdersByUsernameQueryHandler : IRequestHandler<OrdersByUsernameQuery, List<OrderVm>>
+    {
+        private readonly IOrderUow _orderUow;
+        private readonly IMapper _mapper;
+        public OrdersByUsernameQueryHandler(IOrderUow orderUow,
+            IMapper mapper)
+        {
+            _orderUow = orderUow;
+            _mapper = mapper;
+        }
+        public async Task<List<OrderVm>> Handle(OrdersByUsernameQuery request, CancellationToken cancellationToken)
+        {
+            // fetch orders
+            var orders = await _orderUow.GetByUsernameAsync(request.Username);
+            return _mapper.Map<List<OrderVm>>(orders);
+        }
+    }
+}
